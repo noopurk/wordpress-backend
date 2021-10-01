189f0c81133c2b5745e07cfbd18bf71ced78091a
noopurk <noopurkore@gmail.com>
2021-10-02 01:34:49 +0500

    commit pass

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'noopurke' );
 define( 'DB_USER', 'admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Arbaz01@' );
+define( 'DB_PASSWORD', 'noopurkewebs@1' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
